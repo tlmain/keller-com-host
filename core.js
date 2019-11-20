8e7e9b20cdd3004b3154bcb23145ac0732caec0b
lee <lee@debianLaptop>
2019-11-19 22:27:29 -0500

    - start

--- core.js
+++ core.js
@@ -1,1 +1,29 @@
 //init commit
+
+
+
+
+
+
+app.get("/api/:device/receive/bytes", async (_req, _res) => {
+  //Params: count, flag
+  logApi(_req);
+  _res.send();
+});
+
+app.get("/api/:device/receive/string", async (_req, _res) => {
+  logApi(_req);
+  _res.send();
+});
+
+
+app.get("/api/:device/state", async (_req, _res) => {
+  logApi(_req);
+  _res.send();
+});
+
+
+app.get("/api/:device/received", async (_req, _res) => {
+  logApi(_req);
+  _res.send();
+});
